12433e6b9cd62096dc7abfd551e38d5e30fe54af
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2023-12-18 20:02:26 +0100

    list "what to do" js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+
+-1 Chiedo all'utente quanti km dovrà percorrere
+-2 Chiedo all'utente la sua età
+-3 Definisco il prezzo al km
+-4 Definisco il prezzo del biglietto in base ai km percorsi
+-5 Definisco lo sconto applicato per i minorenni
+-6 Definisco lo sconto applicato agli over 65
+-7 Inserisco tutte le informazioni ottenute nel documento 
+*/
+
+
+
+
+
+
